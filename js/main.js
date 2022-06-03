cef918a929581d1f06546d41b7e7aa8e0d0b7f01
Keven Santana <44447313+kevensantana@users.noreply.github.com>
2022-06-03 20:12:51 -0300

    layout PC

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 
+window.addEventListener('scroll', onScroll)
+onScroll()
 
-function onScroll(){   
-    if (scrollY > 0 ) {
+function onScroll() {   
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
       navigation.classList.add('scroll')
-    }else {
+    } else {
       navigation.classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 550) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
     }
 }
 
@@ -15,3 +30,19 @@ function openMenu(){
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+    #home,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content
+`);
